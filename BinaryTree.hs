1491d2a6d77340c8eada34e79ce85d30ae548061
Myria Sarvay <u7644255@anu.edu.au>
2023-12-01 12:37:47 +1100

    Foldable BinaryTree instance

--- BinaryTree.hs
+++ BinaryTree.hs
@@ -6,3 +6,7 @@ data BinaryTree a = Node (BinaryTree a) a (BinaryTree a) | Null
 instance Functor BinaryTree where
     fmap f (Node l x r) = Node (fmap f l) (f x) (fmap f r)
     fmap _ Null         = Null
+
+instance Foldable BinaryTree where
+    foldr f z (Node l x r) = foldr f (x `f` foldr f z r) l
+    foldr _ z Null         = z
