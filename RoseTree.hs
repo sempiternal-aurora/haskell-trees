83b866188ad18b506d95b16f98ba8f1e6128c426
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2024-04-10 19:53:24 +1000

    Implement Traversable RoseTree

--- RoseTree.hs
+++ RoseTree.hs
@@ -9,3 +9,9 @@ instance Functor RoseTree where
 instance Foldable RoseTree where
     -- | Use the list foldable instance to fold over the children of the RoseTree
     foldr f z (Rose r rs) = r `f` foldr (flip (foldr f)) z rs
+
+instance Traversable RoseTree where
+    -- | The list traverse instance is extremely helpful here
+    sequenceA (Rose r rs) = Rose <$> r <*> traverse sequenceA rs
+
+    traverse f (Rose r rs)  = Rose <$> f r <*> traverse (traverse f) rs
