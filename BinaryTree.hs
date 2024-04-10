927efedf3b5a1d72a4877a073fef07e5d51d364b
sempiternal-aurora <78790545+sempiternal-aurora@users.noreply.github.com>
2024-04-10 19:53:24 +1000

    BinaryTree Functor Instance

--- BinaryTree.hs
+++ BinaryTree.hs
@@ -2,3 +2,7 @@ module BinaryTree where
 
 data BinaryTree a = Node (BinaryTree a) a (BinaryTree a) | Null
     deriving (Show, Eq)
+
+instance Functor BinaryTree where
+    fmap f (Node l x r) = Node (fmap f l) (f x) (fmap f r)
+    fmap _ Null         = Null
